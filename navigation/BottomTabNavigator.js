ad9cb2b8f73f929f175f2500f74a40a223394856
marwan5555 <87256789+marwan5555@users.noreply.github.com>
2023-04-17 21:11:27 +0700

    modified:   navigation/BottomTabNavigator.js

--- navigation/BottomTabNavigator.js
+++ navigation/BottomTabNavigator.js
@@ -5,17 +5,58 @@ import Activity from '../screens/Activity';
 import Chat from '../screens/Chat';
 import Feed from '../screens/Feed';
 import Alert from '../screens/Alert';
+import { Ionicons } from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator();
 
 const BottomTabNavigator = () => {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Home" component={Home} />
-      <Tab.Screen name="Chat" component={Chat} />
-      <Tab.Screen name="Activity" component={Activity} />
-      <Tab.Screen name="Feed" component={Feed} />
-      <Tab.Screen name="Alert" component={Alert} />
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home-outline" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Chat"
+        component={Chat}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="chatbox-outline" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Activity"
+        component={Activity}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="list-outline" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Feed"
+        component={Feed}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="newspaper-outline" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Alert"
+        component={Alert}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="notifications-outline" size={size} color={color} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 };
