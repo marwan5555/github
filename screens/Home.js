3ddd6eaac455dd686893bb69f8c83e74022c434a
marwan5555 <87256789+marwan5555@users.noreply.github.com>
2023-04-25 10:41:32 +0700

    new file:   assets/icons/Arrow_Left.png new file:   assets/icons/Favorite.png new file:   assets/icons/Favorite_filled.png new file:   assets/icons/Hamburger.png new file:   assets/icons/Home.png new file:   assets/icons/Notification.png new file:   assets/icons/Search.png new file:   assets/icons/User.png new file:   assets/images/1.jpeg new file:   assets/images/2.jpeg new file:   assets/images/3.jpeg new file:   assets/images/4.jpeg new file:   assets/images/5.jpeg new file:   assets/images/6.jpeg new file:   assets/images/7.jpeg new file:   assets/images/8.jpeg new file:   components/FavoriteButton.js new file:   components/Icon.js new file:   components/MainHeader.js new file:   components/ScreenHeader.js new file:   components/SectionHeader.js new file:   components/TopPlacesCarousel.js new file:   components/TripsList.js new file:   constants/icons.js new file:   constants/theme.js new file:   data/index.js modified:   screens/Home.js

--- screens/Home.js
+++ screens/Home.js
@@ -1,15 +1,40 @@
 import React from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity,ScrollView } from "react-native";
+import {colors} from '../constants/theme';
+import MainHeader from '../components/MainHeader';
+import ScreenHeader from '../components/ScreenHeader';
+import TopPlacesCarousel from '../components/TopPlacesCarousel';
+import {PLACES, TOP_PLACES} from '../data';
+import SectionHeader from '../components/SectionHeader';
+import TripsList from '../components/TripsList';
 import { useNavigation } from "@react-navigation/native";
 
 const Home = () => {
   const navigation = useNavigation();
 
   return (
-    <View>
-      <Text>หน้าหลัก</Text>
+    <View style={styles.container}>
+      {/* <MainHeader title="Travel App" /> ตรงนี้คือเส้นเมนูบนหน้าหลัก */ }
+      <ScreenHeader mainTitle="Dream Trip" secondTitle="สำหรับคุณ" />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <TopPlacesCarousel list={TOP_PLACES} />
+        <SectionHeader
+          title="ทริปตอนนี้"
+          buttonTitle="ทั้งหมด"
+          onPress={() => {}}
+        />
+        <TripsList list={PLACES} />
+      </ScrollView>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.light,
+  },
+});
+
 
 export default Home;
